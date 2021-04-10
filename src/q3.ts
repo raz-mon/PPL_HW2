b5873f4039eb44d3942dab02e6964523e9dc6455
almogze <a199437Z>
2021-04-10 20:44:00 +0300

    class2proc

--- src/q3.ts
+++ src/q3.ts
@@ -1,13 +1,33 @@
-import { /*ClassExp, ProcExp, */ Exp, Program } from "./L31-ast";
+import { ClassExp, ProcExp, Exp, Program, IfExp, makeBoolExp } from "./L31-ast";
 import { Result, makeFailure } from "../shared/result";
+import { is, isEmpty, map } from "ramda";
+import { Binding, CExp, makeAppExp, makeIfExp, makePrimOp, makeProcExp, makeStrExp, VarDecl } from "../imp/L3-ast";
+import { METHODS } from "node:http";
+import { first, rest } from "../shared/list";
+import exp from "node:constants";
 
 /*
 Purpose: Transform ClassExp to ProcExp
 Signature: for2proc(classExp)
 Type: ClassExp => ProcExp
 */
-//export const class2proc = (exp: ClassExp): ProcExp =>
-    //@TODO
+
+export const class2proc = (exp: ClassExp): ProcExp => 
+    isEmpty(exp) ? makeProcExp([],[]):
+    makeProcExp(exp.fields, [(msg: VarDecl) => 
+    makeProcExp(msg,rewriteNewIfExp(msg,map((b: Binding) => b.var, exp.methods)
+    ,map((b: Binding) => b.val, exp.methods)))(msg)]);
+
+export const rewriteNewIfExp = (msg: VarDecl, vars: VarDecl[], vals: CExp[]): CExp => {
+    //  AppExp[eq?, [msg, mtd.name]]
+    let x = makeAppExp(makePrimOp("eq?"), [makeStrExp(msg.var), makeStrExp(first(vars).var)]);
+    if(vars.length === 1 && vals.length === 1){
+        return makeIfExp(x, first(vals), makeBoolExp(false));
+    }else{
+        return makeIfExp(x, first(vals), rewriteNewIfExp(msg, rest(vars), rest(vals)));
+    }
+}
+
 
 /*
 Purpose: Transform L31 AST to L3 AST
